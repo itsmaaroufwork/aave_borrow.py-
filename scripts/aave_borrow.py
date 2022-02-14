83cd15f245fe8c989c276f85dfe042e86c36a274
itsmaaroufwork <itsmaarouf@gmail.com>
2022-02-14 09:43:16 +0100

    lending pool

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -8,3 +8,14 @@ def main():
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
+    lending_pool = get_lending_pool()
+    print(lending_pool)
+
+
+def get_lending_pool():
+    lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_addresses_provider.getLendingPool()
+    lending_pool = interface.ILendingPool(lending_pool_address)
+    return lending_pool
